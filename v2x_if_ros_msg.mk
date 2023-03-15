6c5a357702266312113b77b9edfbf9121e03c9db
Andrew Koerner <andrew.koerner@dlr.de>
2023-03-15 17:27:17 +0100

    WIP: configurable submodules

--- v2x_if_ros_msg.mk
+++ v2x_if_ros_msg.mk
@@ -1,29 +1,36 @@
 # This Makefile contains useful targets that can be included in downstream projects.
 
-ifndef v2x_if_ros_msg_MAKEFILE_PATH
+ifeq ($(filter v2x_if_ros_msg.mk, $(notdir $(MAKEFILE_LIST))), v2x_if_ros_msg.mk)
 
 MAKEFLAGS += --no-print-directory
 
 .EXPORT_ALL_VARIABLES:
-v2x_if_ros_msg_project=v2x_if_ros_msg
-V2X_IF_ROS_MSG_PROJECT=${v2x_if_ros_msg_project}
+V2X_IF_ROS_MSG_PROJECT=v2x_if_ros_msg
 
-v2x_if_ros_msg_MAKEFILE_PATH:=$(shell realpath "$(shell dirname "$(lastword $(MAKEFILE_LIST))")")
-make_gadgets_PATH:=${v2x_if_ros_msg_MAKEFILE_PATH}/make_gadgets
-REPO_DIRECTORY:=${v2x_if_ros_msg_MAKEFILE_PATH}
+V2X_IF_ROS_MSG_MAKEFILE_PATH:=$(shell realpath "$(shell dirname "$(lastword $(MAKEFILE_LIST))")")
+ifeq ($(SUBMODULES_PATH),)
+V2X_IF_ROS_MSG_SUBMODULES_PATH:=${V2X_IF_ROS_MSG_MAKEFILE_PATH}
+else
+V2X_IF_ROS_MSG_SUBMODULES_PATH:=$(shell realpath ${SUBMODULES_PATH})
+endif
+MAKE_GADGETS_PATH:=${V2X_IF_ROS_MSG_SUBMODULES_PATH}/make_gadgets
+ifeq ($(wildcard $(MAKE_GADGETS_PATH)),)
+    $(info INFO: To clone submodules use: 'git submodules update --init --recursive')
+    $(info INFO: To specify alternative path for submodules use: SUBMODULES_PATH="<path to submodules>" make build')
+    $(info INFO: Default submodule path is: ${V2X_IF_ROS_MSG_MAKEFILE_PATH}')
+    $(error "ERROR: ${MAKE_GADGETS_PATH} does not exist. Did you clone the submodules?")
+endif
+REPO_DIRECTORY:=${V2X_IF_ROS_MSG_MAKEFILE_PATH}
 
-v2x_if_ros_msg_tag:=$(shell cd ${make_gadgets_PATH} && make get_sanitized_branch_name REPO_DIRECTORY=${REPO_DIRECTORY})
-V2X_IF_ROS_MSG_TAG=${v2x_if_ros_msg_tag}
+V2X_IF_ROS_MSG_TAG:=$(shell cd ${MAKE_GADGETS_PATH} && make get_sanitized_branch_name REPO_DIRECTORY=${REPO_DIRECTORY})
 
-v2x_if_ros_msg_image=${v2x_if_ros_msg_project}:${v2x_if_ros_msg_tag}
-V2X_IF_ROS_MSG_IMAGE=${v2x_if_ros_msg_image}
+V2X_IF_ROS_MSG_IMAGE=${V2X_IF_ROS_MSG_PROJECT}:${V2X_IF_ROS_MSG_TAG}
 
-v2x_if_ros_msg_CMAKE_BUILD_PATH="${v2x_if_ros_msg_project}/build"
-V2X_IF_ROS_MSG_CMAKE_BUILD_PATH=${v2x_if_ros_msg_CMAKE_BULID_PATH}!
+V2X_IF_ROS_MSG_CMAKE_BUILD_PATH="${V2X_IF_ROS_MSG_PROJECT}/build"
+V2X_IF_ROS_MSG_CMAKE_INSTALL_PATH="${V2X_IF_ROS_MSG_CMAKE_BUILD_PATH}/install"
 
-v2x_if_ros_msg_CMAKE_INSTALL_PATH="${v2x_if_ros_msg_CMAKE_BUILD_PATH}/install"
-V2X_IF_ROS_MSG_CMAKE_INSTALL_PATH=${v2x_if_ros_msg_CMAKE_INSTALL_PATH}
-
+include ${MAKE_GADGETS_PATH}/make_gadgets.mk
+include ${MAKE_GADGETS_PATH}/docker/docker-tools.mk
 
 .PHONY: build_v2x_if_ros_msg 
 build_v2x_if_ros_msg: ## Build v2x_if_ros_msg
